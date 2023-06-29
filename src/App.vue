a97aebae09598755b1780c132c547f93e5f84c41
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-06-29 14:39:24 +0200

    fixed istallation of font source and linked style

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <script>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
+
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  }
+}
 </script>
 
 <template>
-  <h1>Test</h1>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
 </template>
 
-<style></style>
+<style lang="scss"></style>
